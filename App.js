25d0faa7bded5ea418a1f29410fe134421db39d7
Billy Maguire <wmaguire@nexient.com>
2021-04-29 09:51:48 -0400

    Initial UI for ListingDetails component

--- App.js
+++ App.js
@@ -7,31 +7,27 @@ import colors from "./src/config/color";
 import ViewImageScreen from "./src/screens/ViewImageScreen";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
 import Card from "./src/components/Card";
+import ListingDetails from "./src/components/ListingDetails";
 
 export default function App() {
   return (
     // <ViewImageScreen />
     // <WelcomeScreen />
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red Jacket for sale!"
-        subtitle="$100"
-        image={require("./src/assets/jacket.jpg")}
-      />
-    </View>
+    // {/* <Card
+    //   title="Red Jacket for sale!"
+    //   subtitle="$100"
+    //   image={require("./src/assets/jacket.jpg")}
+    // /> */}
+    <ListingDetails
+      image={require("./src/assets/jacket.jpg")}
+      title="Red Jacket For Sale!"
+      subtitle="$100"
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
--- src/components/ListingDetails.js
+++ src/components/ListingDetails.js
@@ -1,8 +1,85 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 
-function ListingDetails(props) {
-  return <View></View>;
+import colors from "../config/color";
+import AppText from "./AppText";
+
+function ListingDetails({ image, title, subtitle }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <View style={styles.details}>
+        <View style={styles.titlesContainer}>
+          <AppText style={styles.title}>{title}</AppText>
+          <AppText style={styles.subtitle}>{subtitle}</AppText>
+        </View>
+        <View style={styles.othersContainer}>
+          <Image
+            style={styles.othersImage}
+            source={require("../assets/mosh.jpg")}
+          />
+          <View style={styles.othersTextContainer}>
+            <AppText style={styles.othersTitle}>Billy Maguire</AppText>
+            <AppText style={styles.othersSubtitle}>5 listings</AppText>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  details: {
+    flex: 2,
+    backgroundColor: colors.white,
+  },
+  image: {
+    flex: 1,
+    overflow: "hidden",
+    height: "100%",
+    width: "100%",
+  },
+  othersContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    backgroundColor: colors.white,
+    marginHorizontal: 15,
+  },
+  othersImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    marginHorizontal: 15,
+  },
+  othersTextContainer: {
+    backgroundColor: colors.white,
+    justifyContent: "flex-start",
+  },
+  othersTitle: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  othersSubtitle: {
+    fontSize: 12,
+    fontWeight: "300",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingBottom: 5,
+  },
+  titlesContainer: {
+    padding: 20,
+  },
+  subtitle: {
+    color: colors.secondary,
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingTop: 5,
+  },
+});
 
 export default ListingDetails;
